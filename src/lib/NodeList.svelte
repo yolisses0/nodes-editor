ada40700963b9c0d95e640e3c1318ad01b7a156f
Yolisses <yolisses0@gmail.com>
2025-01-17 15:34:53 -0300

    Create NodeList

---
+++ src/lib/NodeList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Node } from './Node.js';
+	import NodeItem from './NodeItem.svelte';
+
+	interface Props {
+		nodes: Node[];
+	}
+
+	const { nodes }: Props = $props();
+</script>
+
+<div>
+	{#each nodes as node (node.id)}
+		<NodeItem {node} />
+	{/each}
+</div>
