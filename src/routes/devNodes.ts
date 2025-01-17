ff7e76bd03b0cba6b351d3304c965843d23d5e69
Yolisses <iamurise@gmail.com>
2025-01-17 16:39:22 -0300

    Style ConnectorItemContent

--- src/routes/devNodes.ts
+++ src/routes/devNodes.ts
@@ -6,5 +6,5 @@ const zoom = 20;
 export const devNodes: Node[] = [
 	{ id: 'devNode1', position: new Vector(0 * zoom, 0 * zoom) },
 	{ id: 'devNode2', position: new Vector(3 * zoom, 4 * zoom) },
-	{ id: 'devNode3', position: new Vector(5 * zoom, 6 * zoom) },
+	{ id: 'devNode3', position: new Vector(15 * zoom, 3 * zoom) },
 ];
