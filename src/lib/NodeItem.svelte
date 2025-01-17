5773ca932729636eb5f46ca418885dd3269ac3d4
Yolisses <yolisses0@gmail.com>
2025-01-17 15:32:37 -0300

    Create NodeItem

---
+++ src/lib/NodeItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Node } from "./Node.js";
+
+    interface Props{
+        node:Node
+    }
+
+    const {node}:Props = $props()
+</script>
+
+<div>
+    {node.id}
+</div>
---
+++ src/routes/devNode.ts
@@ -0,0 +1,5 @@
+import type { Node } from '../lib/Node.js';
+
+export const devNode: Node = {
+	id: 'devNode'
+};
