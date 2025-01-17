755da10b3d1e3e25ecfbec2de8f236623db6e123
Yolisses <yolisses0@gmail.com>
2025-01-17 16:01:53 -0300

    Experiment with snippet to pass NodeItem content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import type { Node } from '$lib/Node.js';
 	import NodeList from '$lib/NodeList.svelte';
 	import { devNodes } from './devNodes.js';
 </script>
 
-<NodeList nodes={devNodes} />
+{#snippet content({ node }: { node: Node })}
+	<div>content {node.id}</div>
+{/snippet}
+
+<NodeList nodes={devNodes} {content} />
---
+++ src/routes/NodeItemContent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Node } from '$lib/Node.js';
+
+	interface Props {
+		node: Node;
+	}
+
+	const { node }: Props = $props();
+</script>
+
+<div>
+	{node.id}
+</div>
