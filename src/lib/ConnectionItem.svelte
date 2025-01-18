d0c04a44437efb6037586941730dfdc19c750738
Yolisses <yolisses0@gmail.com>
2025-01-18 10:31:27 -0300

    Implement ConnectionItem without passing position as prop

--- src/lib/ConnectionItem.svelte
+++ src/lib/ConnectionItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Connection } from './Connection.js';
 	import { connectorJointElementPrefix } from './connectorJointElementPrefix.js';
+	import { getElementCenter } from './getElementCenter.js';
 	import { Vector } from './Vector.js';
 	import Wire from './wire/Wire.svelte';
 
@@ -14,13 +15,21 @@
 	let endPosition = $state<Vector>();
 
 	$effect(() => {
-		const { startConnectorId } = connection;
+		const { startConnectorId, endConnectorId } = connection;
 
 		const startConnectorElement = document.getElementById(
 			connectorJointElementPrefix + startConnectorId,
 		);
+		const endConnectorElement = document.getElementById(
+			connectorJointElementPrefix + endConnectorId,
+		);
 
-		console.log(startConnectorElement);
+		if (startConnectorElement) {
+			startPosition = getElementCenter(startConnectorElement);
+		}
+		if (endConnectorElement) {
+			endPosition = getElementCenter(endConnectorElement);
+		}
 	});
 </script>
 
