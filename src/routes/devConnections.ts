4740630c7d5e09afdc00b10ca41641f0ffe322ba
Yolisses <iamurise@gmail.com>
2025-01-18 07:58:52 -0300

    Create VariableSizeComponent

--- src/routes/devConnections.ts
+++ src/routes/devConnections.ts
@@ -2,4 +2,5 @@ import type { Connection } from '$lib/Connection.js';
 
 export const devConnections: Connection[] = [
 	{ id: 'connection1', startConnectorId: 'connector2.1', endConnectorId: 'connector1.1' },
+	{ id: 'connection2', startConnectorId: 'connector3.1', endConnectorId: 'connector1.2' },
 ];
