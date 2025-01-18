f65290eec04d8d21b75e0a85338454566a8095a5
Yolisses <iamurise@gmail.com>
2025-01-18 10:31:27 -0300

    Implement ConnectionItem without passing position as prop

---
+++ src/lib/getElementCenter.ts
@@ -0,0 +1,6 @@
+import { Vector } from './Vector.js';
+
+export function getElementCenter(element: Element): Vector {
+	const rect = element.getBoundingClientRect();
+	return new Vector((rect.left + rect.right) / 2, (rect.top + rect.bottom) / 2);
+}
