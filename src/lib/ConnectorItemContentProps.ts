c4f078ecc92dd961c26d4a4a5c7075492c21949a
Yolisses <iamurise@gmail.com>
2025-01-18 07:06:13 -0300

    Use connector in ConnectorItem

--- src/lib/ConnectorItemContentProps.ts
+++ src/lib/ConnectorItemContentProps.ts
@@ -1,5 +1,7 @@
+import type { Connector } from './Connector.js';
 import type { Vector } from './Vector.js';
 
 export type ConnectorItemContentProps = {
+	connector: Connector;
 	updateConnectorPosition(id: string, position: Vector): void;
 };
