1687a856b5780e87cbf05f8a7994f8fbbf7c30cf
Yolisses <yolisses0@gmail.com>
2025-01-18 16:06:14 -0300

    Implements a move detector in DevPage

--- src/routes/dev/DevPage.svelte
+++ src/routes/dev/DevPage.svelte
@@ -1,49 +1,87 @@
 <script lang="ts">
 	import MovableComponent from './MovableComponent.svelte';
 
+	let counter = $state(0);
 	let content: Element;
 	let container: Element;
+	let savedObserver: IntersectionObserver;
 
-	function getRootMargin() {
+	function getRootMargin(content: Element, container: Element) {
 		const contentRect = content.getBoundingClientRect();
 		const containerRect = container.getBoundingClientRect();
 
-		console.log(contentRect);
-		console.log(containerRect);
-
 		/* top | right | bottom | left */
+		let leftMargin = 1 + containerRect.left - contentRect.left;
+		let topMargin = 1 + containerRect.top - contentRect.top;
+		let rightMargin = 1 + contentRect.right - containerRect.right;
+		let bottomMargin = 1 + contentRect.bottom - containerRect.bottom;
 
-		return (
-			[
-				containerRect.top - contentRect.top, // ok
-				contentRect.right - containerRect.right, // ok
-				contentRect.bottom - containerRect.bottom, // ok
-				containerRect.left - contentRect.left, // ok
-			].join('px ') + 'px'
+		if (!Number.isInteger(leftMargin)) {
+			leftMargin++;
+		}
+		if (!Number.isInteger(topMargin)) {
+			topMargin++;
+		}
+		if (!Number.isInteger(rightMargin)) {
+			rightMargin++;
+		}
+		if (!Number.isInteger(bottomMargin)) {
+			bottomMargin++;
+		}
+
+		console.log({ topMargin, rightMargin, bottomMargin, leftMargin });
+
+		const rootMargin = [topMargin, rightMargin, bottomMargin, leftMargin].join('px ') + 'px';
+		console.log(rootMargin);
+		return rootMargin;
+	}
+
+	function createIntersectionObserver(content: Element, container: Element) {
+		return new IntersectionObserver(
+			(entries, observer) => {
+				counter++;
+				const [entry] = entries;
+
+				console.log(entry.intersectionRatio);
+				console.log('content', content.getBoundingClientRect());
+				console.log('container', container.getBoundingClientRect());
+
+				if (!entry.isIntersecting) {
+					observer.disconnect();
+					const newObserver = createIntersectionObserver(content, container);
+					newObserver.observe(content);
+					savedObserver = newObserver;
+				}
+			},
+			{
+				root: container,
+				threshold: 1,
+				rootMargin: getRootMargin(content, container),
+			},
 		);
 	}
 
 	$effect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				const [entry] = entries;
-				console.log(entry.isIntersecting, entry.boundingClientRect, entry.rootBounds);
-			},
-			{ root: container, rootMargin: getRootMargin(), threshold: 1 },
-		);
-
+		const observer = createIntersectionObserver(content, container);
 		observer.observe(content);
+
+		return () => {
+			if (savedObserver) {
+				savedObserver.disconnect();
+			}
+		};
 	});
 </script>
 
-<div style="padding: 30px;">
-	<div class="container" bind:this={container}>
-		<div class="initial-marker"></div>
-		<MovableComponent>
-			<div bind:this={content} class="content">content</div>
-		</MovableComponent>
-	</div>
+<!-- <div style="padding: 30px;">
+</div> -->
+<div class="container" bind:this={container}>
+	<div class="initial-marker"></div>
+	<MovableComponent>
+		<div bind:this={content} class="content">content</div>
+	</MovableComponent>
 </div>
+{counter}
 
 <style>
 	:global(body, html) {
