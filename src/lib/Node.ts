962c436bb21106667ef42a19892b80b561e89925
Yolisses <iamurise@gmail.com>
2025-01-18 07:02:28 -0300

    Create Connector

--- src/lib/Node.ts
+++ src/lib/Node.ts
@@ -1,6 +1,8 @@
+import type { Connector } from './Connector.js';
 import type { Vector } from './Vector.js';
 
 export interface Node {
 	id: string;
 	position: Vector;
+	connectors: Connector[];
 }
