31a69c1b889aa5f45ad92f2781a11137811843c3
Yolisses <iamurise@gmail.com>
2025-01-18 10:35:50 -0300

    Clean DevPage

--- src/routes/dev/DevPage.svelte
+++ src/routes/dev/DevPage.svelte
@@ -2,35 +2,12 @@
 	import MovableComponent from './MovableComponent.svelte';
 
 	let content = $state<Element>();
-	let container = $state<Element>();
-
-	$effect(() => {
-		const observer = new ResizeObserver((entries) => {
-			console.log(entries[0].target, entries[0].contentRect);
-		});
-
-		if (content) {
-			observer.observe(content);
-			console.log('observe');
-		}
-
-		return () => {
-			if (content) {
-				observer.unobserve(content);
-				console.log('unobserve');
-			}
-		};
-	});
 </script>
 
-<div style="height: 2000px; background-color: red;"></div>
-<div bind:this={container} style="min-height: 600px;">
-	container
-	<MovableComponent bind:element={content} {container} />
-</div>
+<MovableComponent bind:element={content} />
 
 <style>
-	div {
-		outline: 2px solid blue;
+	:global(body, html) {
+		margin: 0;
 	}
 </style>
