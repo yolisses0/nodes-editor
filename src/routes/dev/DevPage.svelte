601c869b1d388dbca3fed519f157f98a7032d54a
Yolisses <yolisses0@gmail.com>
2025-01-18 14:38:29 -0300

    Experiment with intersection observer movement detection

--- src/routes/dev/DevPage.svelte
+++ src/routes/dev/DevPage.svelte
@@ -1,31 +1,73 @@
 <script lang="ts">
-	import { MoveObserver } from '$lib/position/MoveObserver.js';
 	import MovableComponent from './MovableComponent.svelte';
 
 	let content: Element;
 	let container: Element;
 
-	$effect(() => {
-		const observer = new MoveObserver(
-			(entries) => {
-				console.log(entries);
-			},
-			{ root: container },
-		);
-		observer.observe(content);
+	function getRootMargin() {
+		const contentRect = content.getBoundingClientRect();
+		const containerRect = container.getBoundingClientRect();
 
-		return () => observer.unobserve(content);
+		console.log(contentRect);
+		console.log(containerRect);
+
+		/* top | right | bottom | left */
+
+		return (
+			[
+				containerRect.top - contentRect.top, // ok
+				contentRect.right - containerRect.right, // ok
+				contentRect.bottom - containerRect.bottom, // ok
+				containerRect.left - contentRect.left, // ok
+			].join('px ') + 'px'
+		);
+	}
+
+	$effect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				const [entry] = entries;
+				console.log(entry.isIntersecting, entry.boundingClientRect, entry.rootBounds);
+			},
+			{ root: container, rootMargin: getRootMargin(), threshold: 1 },
+		);
+
+		observer.observe(content);
 	});
 </script>
 
-<div style="position: relative;" bind:this={container}>
-	<MovableComponent>
-		<div bind:this={content}>content</div>
-	</MovableComponent>
+<div style="padding: 30px;">
+	<div class="container" bind:this={container}>
+		<div class="initial-marker"></div>
+		<MovableComponent>
+			<div bind:this={content} class="content">content</div>
+		</MovableComponent>
+	</div>
 </div>
 
 <style>
 	:global(body, html) {
 		margin: 0;
+	}
+
+	.initial-marker {
+		width: 50px;
+		height: 20px;
+		position: absolute;
+		outline: 1px solid green;
+		top: 10px;
+		left: 10px;
+	}
+
+	.content {
+		width: 50px;
+		height: 20px;
+	}
+
+	.container {
+		width: 100px;
+		height: 300px;
+		position: relative;
+		background-color: red;
 	}
 </style>
