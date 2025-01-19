67ee3dfff2ca05a57b51151d18fe721e7be6badb
Yolisses <iamurise@gmail.com>
2025-01-19 13:28:27 -0300

    Create ConnectorArea

---
+++ src/lib/connector/ConnectorArea.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	const { children }: Props = $props();
+
+	function handleMouseEnter(e: MouseEvent) {
+		console.log('mouse enter');
+	}
+</script>
+
+<button onmouseenter={handleMouseEnter}>
+	{@render children()}
+</button>
