17e8bc48bdb7ec613255c8f5a6fc40a9c1dab780
Yolisses <iamurise@gmail.com>
2025-01-19 12:04:15 -0300

    Experiment with context in DevPage

---
+++ src/routes/dev/Parent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import Child from './Child.svelte';
+
+	const devContext = $state({ value: 2 });
+	setContext('devValue', devContext);
+
+	function handleClick() {
+		devContext.value += 1;
+	}
+</script>
+
+<div>
+	parent value {devContext.value}
+</div>
+<button onclick={handleClick}>increase</button>
+<Child />
