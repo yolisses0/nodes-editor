28ab923aca0abfcd20369ce6d1280262f61ccf52
Yolisses <yolisses0@gmail.com>
2025-01-19 10:11:11 -0300

    Replace HOC by wrappers

--- src/lib/node/NodeList.svelte
+++ src/lib/node/NodeList.svelte
@@ -2,31 +2,19 @@
 	import type { ConnectorPositions } from '$lib/connector/ConnectorPositions.js';
 	import { setConnectorPositionsContext } from '$lib/connector/connectorPositionsContext.js';
 	import type { Snippet } from 'svelte';
-	import type { Connection } from '../connection/Connection.js';
-	import ConnectionItem from '../connection/ConnectionItem.svelte';
-	import type { Node } from './Node.js';
-	import NodeItem from './NodeItem.svelte';
-	import type { NodeItemContentProps } from './NodeItemContentProps.js';
 
 	interface Props {
-		nodes: Node[];
-		connections: Connection[];
-		content: Snippet<[NodeItemContentProps]>;
+		children: Snippet;
 	}
 
-	const { nodes, content, connections }: Props = $props();
+	const { children }: Props = $props();
 
 	const connectorPositions: ConnectorPositions = $state({});
 	setConnectorPositionsContext(connectorPositions);
 </script>
 
 <div class="node-list">
-	{#each nodes as node (node.id)}
-		<NodeItem {node} {content} />
-	{/each}
-	{#each connections as connection (connection.id)}
-		<ConnectionItem {connection} />
-	{/each}
+	{@render children()}
 </div>
 
 <style>
---
+++ src/routes/CustomNodeList.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Connection } from '$lib/connection/Connection.js';
+	import ConnectionItem from '$lib/connection/ConnectionItem.svelte';
+	import type { Node } from '$lib/node/Node.js';
+	import NodeList from '$lib/node/NodeList.svelte';
+	import CustomNodeItem from './CustomNodeItem.svelte';
+
+	interface Props {
+		nodes: Node[];
+		connections: Connection[];
+	}
+
+	const { nodes, connections }: Props = $props();
+</script>
+
+<NodeList>
+	{#each nodes as node (node.id)}
+		<CustomNodeItem {node} />
+	{/each}
+	{#each connections as connection (connection.id)}
+		<ConnectionItem {connection} />
+	{/each}
+</NodeList>
