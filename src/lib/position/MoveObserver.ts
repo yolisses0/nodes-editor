7f76460377fc235964306e9b91535c9cd821de18
Yolisses <yolisses0@gmail.com>
2025-01-19 07:18:43 -0300

    Implement MoveObserver basic logic

--- src/lib/position/MoveObserver.ts
+++ src/lib/position/MoveObserver.ts
@@ -2,41 +2,81 @@ import type { MoveObserverCallback } from './MoveObserverCallback.js';
 import type { MoveObserverInit } from './MoveObserverInit.js';
 
 export class MoveObserver {
-	observationTargets: Element[] = [];
+	root: Element;
+	intersectionObserver?: IntersectionObserver;
 
 	constructor(
 		public callback: MoveObserverCallback,
 		public options: MoveObserverInit,
-	) {}
-
-	observe(target: Element) {
-		const overlay = document.createElement('div');
-		overlay.style.position = 'absolute';
-
-		const rootRect = this.options.root.getBoundingClientRect();
-		const targetRect = target.getBoundingClientRect();
-
-		overlay.style.background = '#f008';
-		overlay.style.pointerEvents = 'none';
-		overlay.style.width = targetRect.width + 'px';
-		overlay.style.height = targetRect.height + 'px';
-		overlay.style.top = targetRect.top - rootRect.top + 'px';
-		overlay.style.left = targetRect.left - rootRect.left + 'px';
-
-		this.options.root.appendChild(overlay);
-
-		const intersectionObserver = new IntersectionObserver(
-			(entries) => {
-				console.log(entries);
-			},
-			{ root: target },
-		);
-		intersectionObserver.observe(overlay);
+	) {
+		this.root = options.root;
 	}
 
-	unobserve(target: Element) {}
+	getRootMargin(target: Element, root: Element) {
+		const contentRect = target.getBoundingClientRect();
+		const containerRect = root.getBoundingClientRect();
+
+		let topMargin = 1 + containerRect.top - contentRect.top;
+		let leftMargin = 1 + containerRect.left - contentRect.left;
+		let rightMargin = 1 + contentRect.right - containerRect.right;
+		let bottomMargin = 1 + contentRect.bottom - containerRect.bottom;
+
+		if (!Number.isInteger(topMargin)) {
+			topMargin++;
+		}
+		if (!Number.isInteger(leftMargin)) {
+			leftMargin++;
+		}
+		if (!Number.isInteger(rightMargin)) {
+			rightMargin++;
+		}
+		if (!Number.isInteger(bottomMargin)) {
+			bottomMargin++;
+		}
+
+		/* top | right | bottom | left */
+		const rootMargin = [topMargin, rightMargin, bottomMargin, leftMargin].join('px ') + 'px';
+		return rootMargin;
+	}
+
+	createIntersectionObserver(target: Element, root: Element) {
+		return new IntersectionObserver(
+			(entries, observer) => {
+				const [entry] = entries;
+
+				if (!entry.isIntersecting) {
+					observer.disconnect();
+					this.intersectionObserver = this.createIntersectionObserver(target, root);
+					this.intersectionObserver.observe(target);
+				}
+
+				console.log('update');
+				if (entry.isIntersecting) {
+					this.callback([], this);
+				}
+			},
+			{
+				root: root,
+				threshold: 1,
+				rootMargin: this.getRootMargin(target, root),
+			},
+		);
+	}
+
+	observe(target: Element) {
+		if (this.intersectionObserver) {
+			throw new Error('Observe is still not implemented for multiple targets');
+		}
+
+		this.intersectionObserver = this.createIntersectionObserver(target, this.root);
+		this.intersectionObserver.observe(target);
+	}
+
+	unobserve(target: Element) {
+		throw new Error('Not implemented');
+	}
 
 	disconnect() {
-		this.observationTargets = [];
+		this.intersectionObserver?.disconnect();
 	}
 }
