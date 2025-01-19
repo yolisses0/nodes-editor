356a3cab7afb2ce5872f7f5f9aff5195405db9fa
Yolisses <iamurise@gmail.com>
2025-01-19 10:11:11 -0300

    Replace HOC by wrappers

--- src/routes/NodesPage.svelte
+++ src/routes/NodesPage.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-	import type { NodeItemContentProps } from '$lib/node/NodeItemContentProps.js';
-	import NodeList from '$lib/node/NodeList.svelte';
+	import CustomNodeList from './CustomNodeList.svelte';
 	import { devConnections } from './devConnections.js';
 	import { devNodes } from './devNodes.js';
-	import NodeItemContent from './NodeItemContent.svelte';
 </script>
 
-{#snippet content({ node }: NodeItemContentProps)}
-	<NodeItemContent {node} />
-{/snippet}
-
-<NodeList {content} nodes={devNodes} connections={devConnections} />
+<CustomNodeList nodes={devNodes} connections={devConnections} />
