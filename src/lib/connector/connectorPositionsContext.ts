e5d9f9861a3fba4c0972baf75fce8aa47e94b5ab
Yolisses <iamurise@gmail.com>
2025-01-19 09:35:39 -0300

    Create connectorPositionsContext

---
+++ src/lib/connector/connectorPositionsContext.ts
@@ -0,0 +1,12 @@
+import { getContext, setContext } from 'svelte';
+import type { ConnectorPositions } from './ConnectorPositions.js';
+
+const connectorPositionsKey = 'connectorPositionsKey';
+
+export function setConnectorPositionsContext(connectorPositions: ConnectorPositions) {
+	return setContext(connectorPositionsKey, connectorPositions);
+}
+
+export function getConnectorPositionsContext() {
+	return getContext(connectorPositionsKey);
+}
