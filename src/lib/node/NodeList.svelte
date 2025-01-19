b05da571cbf32a3e488800b3d97539bd34c00e52
Yolisses <yolisses0@gmail.com>
2025-01-19 12:08:20 -0300

    Fix node list context

--- src/lib/node/NodeList.svelte
+++ src/lib/node/NodeList.svelte
@@ -2,7 +2,7 @@
 	import type { ConnectorPositions } from '$lib/connector/ConnectorPositions.js';
 	import { setConnectorPositionsContext } from '$lib/connector/connectorPositionsContext.js';
 	import type { Snippet } from 'svelte';
-	import { setNodeListContext } from './nodeListContext.svelte.js';
+	import { setNodeListContext, type NodeListContext } from './nodeListContext.svelte.js';
 
 	interface Props {
 		children: Snippet;
@@ -12,13 +12,13 @@
 	const connectorPositions: ConnectorPositions = $state({});
 	setConnectorPositionsContext(connectorPositions);
 
-	let element = $state<Element>();
-	$effect(() => {
-		setNodeListContext(element);
+	const nodeListContext: NodeListContext = $state({
+		nodeList: undefined,
 	});
+	setNodeListContext(nodeListContext);
 </script>
 
-<div class="node-list" bind:this={element}>
+<div class="node-list" bind:this={nodeListContext.nodeList}>
 	{@render children()}
 </div>
 
