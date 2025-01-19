7d5da55b58e79068e27937414a14d893bf511032
Yolisses <yolisses0@gmail.com>
2025-01-19 10:04:15 -0300

    Separate NodesPage

---
+++ src/routes/NodesPage.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { NodeItemContentProps } from '$lib/node/NodeItemContentProps.js';
+	import NodeList from '$lib/node/NodeList.svelte';
+	import { devConnections } from './devConnections.js';
+	import { devNodes } from './devNodes.js';
+	import NodeItemContent from './NodeItemContent.svelte';
+</script>
+
+{#snippet content({ node }: NodeItemContentProps)}
+	<NodeItemContent {node} />
+{/snippet}
+
+<NodeList {content} nodes={devNodes} connections={devConnections} />
