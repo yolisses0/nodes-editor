576c029b42d3bee79e18bd2a425f050ff0860109
Yolisses <iamurise@gmail.com>
2025-01-20 06:48:00 -0300

    Use just position instead of node in NodeItem

--- src/lib/node/NodeItem.svelte
+++ src/lib/node/NodeItem.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
+	import type { Vector } from '$lib/space/Vector.js';
 	import type { Snippet } from 'svelte';
-	import type { Node } from './Node.js';
 
 	interface Props {
-		node: Node;
+		position: Vector;
 		children: Snippet;
 	}
 
-	const { node, children }: Props = $props();
+	const { children, position }: Props = $props();
 </script>
 
-<div class="node-item" style:left={node.position.x + 'px'} style:top={node.position.y + 'px'}>
+<div class="node-item" style:left={position.x + 'px'} style:top={position.y + 'px'}>
 	{@render children()}
 </div>
 
