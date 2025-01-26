4ed392fef0d10df322d05276ee225c54bbbdbcdd
Yolisses <iamurise@gmail.com>
2025-01-26 17:52:05 -0300

    Remove some logs

--- src/examples/CustomNodeList.svelte
+++ src/examples/CustomNodeList.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import { getPreviewConnectionContext } from '$lib/connection/previewConnectionContext.js';
-	import {
-		ConnectionItem,
-		getNodeListContext,
-		NodeList,
-		PreviewConnectionWire,
-		type Connection,
-	} from '$lib/index.js';
-	import { getMouseContext } from '$lib/mouse/mouseContext.js';
+	import { ConnectionItem, NodeList, PreviewConnectionWire, type Connection } from '$lib/index.js';
 	import { PreviewConnectionPointerStrategy } from '$lib/node/PreviewConnectionPointerStrategy.svelte.js';
 	import type { CustomNode } from './CustomNode.svelte.js';
 	import CustomNodeItem from './CustomNodeItem.svelte';
@@ -21,15 +13,7 @@
 
 	const { customNodes, connections }: Props = $props();
 
-	const mouseContext = getMouseContext();
-	const nodeListContext = getNodeListContext();
-	const previewConnectionContext = getPreviewConnectionContext();
-
-	const previewConnectionPointerStrategy = $derived(
-		new PreviewConnectionPointerStrategy((e) => console.log(e)),
-	);
-	$inspect(nodeListContext.nodeList);
-	$inspect(previewConnectionPointerStrategy);
+	const previewConnectionPointerStrategy = $derived(new PreviewConnectionPointerStrategy(() => {}));
 </script>
 
 <NodeList pointerStrategy={previewConnectionPointerStrategy}>
