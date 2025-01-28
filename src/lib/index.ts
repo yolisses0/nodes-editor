40713b19baebbbcb100c02dd6257bf41610eb401
Yolisses <yolisses0@gmail.com>
2025-01-28 07:07:40 -0300

    Export context functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,24 +1,40 @@
 import type { Connection } from './connection/Connection.js';
+import ConnectionItem from './connection/ConnectionItem.svelte';
+import {
+	getPreviewConnectionContext,
+	setPreviewConnectionContext,
+} from './connection/previewConnectionContext.js';
+import { PreviewConnectionPointerStrategy } from './connection/PreviewConnectionPointerStrategy.js';
+import PreviewConnectionWire from './connection/PreviewConnectionWire.svelte';
 import type { Connector } from './connector/Connector.js';
+import ConnectorArea from './connector/ConnectorArea.svelte';
+import ConnectorItem from './connector/ConnectorItem.svelte';
+import {
+	getConnectorPositionsContext,
+	setConnectorPositionsContext,
+} from './connector/connectorPositionsContext.js';
+import { setDefaultContexts } from './contexts/setDefaultContexts.svelte.js';
+import { getMouseContext, setMouseContext } from './mouse/mouseContext.js';
 import type { EndPreviewConnectionEvent } from './node/events/EndPreviewConnectionEvent.js';
 import type { MoveEvent } from './node/events/MoveEvent.js';
+import Mover from './node/Mover.svelte';
 import type { Node } from './node/Node.js';
+import NodeItem from './node/NodeItem.svelte';
+import NodeList from './node/NodeList.svelte';
 import { getNodeListContext, setNodeListContext } from './node/nodeListContext.js';
+import { getNodeRectsContext, setNodeRectsContext } from './node/nodeRectsContext.js';
+import {
+	getSelectedNodesContext,
+	setSelectedNodesContext,
+} from './selection/selectedNodesContext.js';
+import { getSelectionBoxContext, setSelectionBoxContext } from './selection/selectionBoxContext.js';
+import { SelectionBoxPointerStrategy } from './selection/SelectionBoxPointerStrategy.js';
 import { Vector } from './space/Vector.js';
 import { getElementPosition } from './ui/getElementPosition.js';
 import { getMouseRelativePosition } from './ui/getMouseRelativePosition.js';
 import { getLinePathD } from './wire/getLinePath.js';
 import { getVectorsString } from './wire/getVectorsString.js';
 import { getVectorString } from './wire/getVectorString.js';
-
-// Yes, they look broken
-import ConnectionItem from './connection/ConnectionItem.svelte';
-import PreviewConnectionWire from './connection/PreviewConnectionWire.svelte';
-import ConnectorArea from './connector/ConnectorArea.svelte';
-import ConnectorItem from './connector/ConnectorItem.svelte';
-import Mover from './node/Mover.svelte';
-import NodeItem from './node/NodeItem.svelte';
-import NodeList from './node/NodeList.svelte';
 import WirePath from './wire/WirePath.svelte';
 import WireSvg from './wire/WireSvg.svelte';
 
@@ -26,17 +42,32 @@ export {
 	ConnectionItem,
 	ConnectorArea,
 	ConnectorItem,
+	getConnectorPositionsContext,
 	getElementPosition,
 	getLinePathD,
+	getMouseContext,
 	getMouseRelativePosition,
 	getNodeListContext,
+	getNodeRectsContext,
+	getPreviewConnectionContext,
+	getSelectedNodesContext,
+	getSelectionBoxContext,
 	getVectorsString,
 	getVectorString,
 	Mover,
 	NodeItem,
 	NodeList,
+	PreviewConnectionPointerStrategy,
 	PreviewConnectionWire,
+	SelectionBoxPointerStrategy,
+	setConnectorPositionsContext,
+	setDefaultContexts,
+	setMouseContext,
 	setNodeListContext,
+	setNodeRectsContext,
+	setPreviewConnectionContext,
+	setSelectedNodesContext,
+	setSelectionBoxContext,
 	Vector,
 	WirePath,
 	WireSvg,
